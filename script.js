40b04c6c0569f82c485dbba205c9d9b5896db7f5
mrndolo <callistomutunga@gmail.com>
2024-07-05 21:39:36 +0300

    Added drag icon and set its position

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var imgBox = document.querySelector(".img-box");
 var imgWrap = document.querySelector(".img-wrap");
 var originalImg = document.getElementById("originalImg");
+var line = document.getElementById("line");
 
 originalImg.style.width = imgBox.offsetWidth + "px";
 
@@ -9,4 +10,5 @@ var leftSpace = imgBox.offsetLeft;
 imgBox.onmousemove = function(e){
     var boxWidth = (e.pageX - leftSpace) + "px";
     imgWrap.style.width = boxWidth;
+    line.style.left = boxWidth;
 }
